2147848aec045f60ddc819a5ac49f2499f44bfd8
Vijay Keerthan <vijay.keerthan@abjayon.com>
2020-11-09 17:35:29 +0530

    javascript objects

---
+++ jslesson90.js
@@ -0,0 +1,24 @@
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist":"Charlie Puth",
+    "title":"we dont talk anymore",
+    "release_year": 2018,
+    "formats":[
+      "ss",
+      "mm",
+      "ms"
+    ]
+  }
+  // Add a record here
+];
