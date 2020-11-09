c0a6cb9803363dafd18ad78892bf0508c412abf4
Vijay Keerthan <vijay.keerthan@abjayon.com>
2020-11-09 16:26:46 +0530

    if else conditions

---
+++ jslesson80.js
@@ -0,0 +1,25 @@
+var count = 0;
+
+function cc(card) {
+  // Only change code below this line
+  if(card==2 || card==3 || card==4 || card==5 || card==6){
+    count += 1;
+  }
+  else if(card==7 || card==8 || card==9){
+    
+  }
+  else{
+    count -= 1;
+  }
+  
+  if(count>0){
+    return count+" Bet";
+  }
+  else{
+    return count+" Hold";
+  }
+
+  // Only change code above this line
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
